c9cd9bd49f94e8b4f6d8d78c5c5511290fdb22d1
Vaaiibhav Vaidya <vaaiibhav@live.com>
2020-01-07 08:33:15 +0530

    created Bulb program

---
+++ assets/js/bulb.js
@@ -0,0 +1,18 @@
+function turnBulbOff(){
+        document.getElementById("bulbImg").src= "assets/images/pic_bulboff.gif"
+
+}
+
+function turnBulbOn(){
+
+    document.getElementById("bulbImg").src= "assets/images/pic_bulbon.gif"
+
+
+}
+var x = 24;
+
+var z = 14;
+
+var y = "Heelo Class";
+
+
